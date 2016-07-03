c997988053cdb05c0a97cad7c0e1afca3880280d
Jan <Jan@192.168.0.115>
2016-07-02 21:01:38 -0700

    added int x.

--- GitTest/src/gittest/GitTest.java
+++ GitTest/src/gittest/GitTest.java
@@ -9,9 +9,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        /*
-        *  need methods
-        */
+        int x = 2;
     }
     
 }
