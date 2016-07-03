a3bdc8c96245fd399027828b1af3106bdd7b2fb6
Jan Buzek <Jan Buzek>
2016-07-02 20:49:41 -0700

    Comittted Hello World. Simple display. comitted hello world.

---
+++ GitTest/src/gittest/GitTest.java
@@ -0,0 +1,14 @@
+
+package gittest;
+
+/**
+ *
+ * @author Jan
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
